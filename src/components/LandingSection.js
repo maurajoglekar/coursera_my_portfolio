df4e985502a9ddce2ac09f6f26f9f8f0fcd1515e
maurajoglekar <31258321+maurajoglekar@users.noreply.github.com>
2024-01-20 21:09:42 -0800

    changed landing section

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Maura!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -16,7 +16,7 @@ const LandingSection = () => (
     backgroundColor="#2A4365"
   >
     <VStack w="1024px" p={32} alignItems="center">
-      <Avatar name="avatar-pic" src="https://i.pravatar.cc/150?img=7" />
+      <Avatar name="avatar-pic" src="https://i.pravatar.cc/150?img=41" />
       <Heading as="h4" size="sm">
         {greeting}
       </Heading>
